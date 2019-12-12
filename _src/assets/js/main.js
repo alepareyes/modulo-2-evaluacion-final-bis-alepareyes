e7d91e2b4fa5d34ecfcf29e92f02674fba6e5c21
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-12 19:28:28 +0100

    add new  request info api

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,68 +1,97 @@
 'use strict';
 
 
-// const inputs = document.querySelectorAll('.js-input');
+const inputs = document.querySelectorAll('.js-input');
 
-const option4 = document.querySelector('.js-input4');
-const option6 = document.querySelector('.js-input6');
-const option8 = document.querySelector('.js-input8');
+// const option4 = document.querySelector('.js-input4');
+// const option6 = document.querySelector('.js-input6');
+// const option8 = document.querySelector('.js-input8');
 
 const cardsContainer = document.querySelector('.js-cards-container');
 const startButton = document.querySelector('.js-btn');
 
 const url = 'https://beta.adalab.es/ejercicios-extra/api/pokemon-cards/'
 
-/* 
-function setDefaultCards() {
-    const currentOption = optionFour;
-    currentOption.checked = true;
-    currentOption.click();
-} */
 
 
-function getServerCards() { //Lo intenté recorriendo el array y metiendo el inputvalue en el url pero no funcionaba.
+//////////////////////////////////////  INPUT POR DEFECTO
+function setDefaultInput() {
+    const currentInput = document.querySelector('.input4');
+    currentInput.checked = true;
+    currentInput.click(); // FALSO CLICK.
+}
 
-    if (option4.checked) {
-        fetch(`${url}${option4.value}.json`)
-            .then(response => response.json())
-            .then(data => createCard(data))
 
-    }
-    if (option6.checked) {
-        fetch(`${url}${option6.value}.json`)
-            .then(response => response.json())
-            .then(data => createCard(data))
-    }
 
-    if (option8.checked) {
-        fetch(`${url}${option8.value}.json`)
-            .then(response => response.json())
-            .then(data => createCard(data))
+/////////////////////////////////////   PINTAR CARTAS SOLICITADAS
+function getServerCards() {
+
+    cardsContainer.innerHTML = "";
+
+    for (const input of inputs) {
+        const inputValue = input.value;
+        const clickedInput = input.checked;
+
+        if (clickedInput === true) {
+            fetch(`${url}${inputValue}.json`)
+                .then(response => response.json())
+                .then(data => createCard(data))
+        }
     }
 };
 
 
 
+////////////////////////////////////////   CREACIÓN DE LA CARTA (contenedores y clases)
 function createCard(cards) {
     for (const card of cards) {
+
         const carta = document.createElement('div');
         carta.setAttribute('class', '.js-card card');
+
 
         const img = document.createElement('img');
         img.setAttribute('src', card.image);
         img.setAttribute('class', '.js-img img hidden');
 
+
         const imgFlip = document.createElement('p');
         imgFlip.setAttribute('class', '.js-flip flip')
-        const alepa = document.createTextNode('Ale Reyes');
+        const alepa = document.createTextNode("Who's That Pokémon?");
         imgFlip.appendChild(alepa);
+
 
         carta.appendChild(img);
         cardsContainer.appendChild(carta);
+        carta.appendChild(imgFlip);
     }
 }
 
 
-startButton.addEventListener('click', getServerCards);
+function stopBotton(ev) {
+    ev.preventDefault();
+}
 
-// setDefaultCards();
+
+// function flipCard() {
+//     carta.classList.add('.hidden');
+
+// }
+
+
+// function listenCards() {
+
+//     const itemCards = document.querySelectorAll('.js-card');
+
+//     for (const item of itemCards) {
+//         item.addEventListener('click', flipCard);
+//     }
+// };
+
+
+
+
+startButton.addEventListener('click', getServerCards);
+startButton.addEventListener('click', stopBotton);
+
+setDefaultInput();
